379bb99e72621854c1224b289f30dac74e92556e
Jordi Kroon <jkroon@tde.nl>
2018-04-30 16:24:58 +0200

    Prepare for webpack 4

--- webpack/plugins/css-plugin.js
+++ webpack/plugins/css-plugin.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssNano = require('cssnano');
 
 module.exports = function (webpack, config) {
   webpack.module.rules.push({
     test: /\.scss$/,
-    use: ExtractTextPlugin.extract({
-      use: [{
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
         loader: 'css-loader',
         options: {
           sourceMap: config.debug,
@@ -26,14 +27,12 @@ module.exports = function (webpack, config) {
         options: {
           sourceMap: config.debug,
         },
-      }],
-      fallback: 'style-loader',
-    }),
+      },
+    ],
   });
 
-  webpack.plugins.push(new ExtractTextPlugin({
+  webpack.plugins.push(new MiniCssExtractPlugin({
     filename: config.components.css.fileMask,
-    allChunks: true,
   }));
 
   if (!config.debug) {
