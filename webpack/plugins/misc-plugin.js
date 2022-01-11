01f12c5b2abfadec1a6d37eba1ea1fbfbc63e37f
John van Hulsen <jvhulsen@tde.nl>
2022-01-11 22:02:09 +0100

    Update package.json with new packages, installed peer dependencies and update friendly-errors-webpack-plugin

--- webpack/plugins/misc-plugin.js
+++ webpack/plugins/misc-plugin.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies,no-new */
 const path = require('path');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('@nuxt/friendly-errors-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-varsO
 module.exports = function (webpack, config) {
   webpack.plugins.push(new WebpackNotifierPlugin({
     alwaysNotify: true,
