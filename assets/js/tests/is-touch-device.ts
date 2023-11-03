adda886b875d80b128109160dd1893a0e10b4233
Jeffrey Schmitz <jschmitz@tde.nl>
2023-11-03 08:44:29 +0100

    update test for touch device

--- assets/js/tests/is-touch-device.ts
+++ assets/js/tests/is-touch-device.ts
@@ -1,11 +1,1 @@
-export const isTouchDevice = (): boolean => {
-  const msGesture =
-    window.navigator && (window as any).navigator.msMaxTouchPoints && (window as any).MSGesture;
-
-  const touch =
-    'ontouchstart' in window ||
-    msGesture ||
-    ((window as any).DocumentTouch && document instanceof (window as any).DocumentTouch);
-
-  return !!touch;
-};
+export const isTouchDevice = (): boolean => 'ontouchstart' in document.documentElement;
