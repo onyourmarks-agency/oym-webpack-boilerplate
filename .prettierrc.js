1802e8e0fde897c903c19ab5902cf48f36fc0db9
John van Hulsen <jvhulsen@onyourmarks.agency>
2024-03-21 09:10:10 +0100

    build(prettier): add svelte option to prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,19 @@ module.exports = {
   bracketSameLine: true,
   cssDeclarationSorterOrder: process.env.ORDER ? process.env.ORDER : 'smacss',
   htmlWhitespaceSensitivity: 'ignore',
-  plugins: ['prettier-plugin-css-order'],
+  plugins: [
+    'prettier-plugin-css-order',
+    'prettier-plugin-svelte',
+  ],
   printWidth: 100,
   semi: true,
   singleQuote: true,
+  overrides: [
+    {
+      files: '*.svelte',
+      options: {
+        parser: 'svelte',
+      },
+    },
+  ],
 };
